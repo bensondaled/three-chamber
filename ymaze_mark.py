a169769518fd999b84b57c4086d0c0f2050735f8
Ben <bendeverett@gmail.com>
2015-01-18 21:40:10 -0500

    important logic fix

--- ymaze_mark.py
+++ ymaze_mark.py
@@ -167,7 +167,7 @@ class Marker(object):
         self.score = 'none'
         for t in self.transitions:
             if self.score == 'correct':
-                if t['to'] not in [self.correct+2, self.C]:
+                if t['to'] not in [self.correct+2, self.correct, self.C]:
                     self.score = 'null'
                     break
             elif t['to'] == self.correct:
