16d7b85c6a637d1908c72d112c53dda0ba59afbf
Ben <bendeverett@gmail.com>
2015-01-19 14:02:06 -0500

    saving paths

--- ymaze_mark.py
+++ ymaze_mark.py
@@ -40,8 +40,8 @@ class Marker(object):
 
     def end(self):
         self.results = dict(n=self.n, score=self.score, transitions=self.transitions, room_key=self.room_key, time_to_correct=self.time_to_correct, distance=self.distance, start_time=self.start_time, start_idx=self.start_idx)
-        np.savez(self.fh.make_path('behaviour.npz',mode=0), **self.results)
-        savemat(self.fh.make_path('behaviour.mat',mode=0), self.results)
+        np.savez(self.fh.make_path('behaviour.npz',mode=1), **self.results)
+        savemat(self.fh.make_path('behaviour.mat',mode=1), self.results)
     def man_update(self, d):
         for k,v in d.items():
             setattr(self,k,v)
